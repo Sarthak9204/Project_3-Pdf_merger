744f25458ed4f15979b8110729dcbeebf86c1e35
Sarthak9204 <suranasarthak2004@gmail.com>
2023-09-11 20:23:27 +0530

    Added exception handling

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import PyPDF2
 pdfs = ['pdf1.pdf', 'pdf2.pdf']
 
 merger = PyPDF2.PdfMerger()
-
-for pdf in pdfs:
-    merger.append(pdf)
-
+try:
+    for pdf in pdfs:
+        merger.append(pdf)
+except Exception as e:
+    print("Error :- ", e)
 merger.write("result.pdf")
 merger.close()
